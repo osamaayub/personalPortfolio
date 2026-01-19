544147b17a5863f119929377508e47f65704bdfa
osamaayub <ayubosama416.com>
2026-01-19 23:27:39 +0500

    updated about & intro experience section

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,6 @@
-import { NextConfig } from "next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // REQUIRED for GitHub Pages
-  images: {
-    unoptimized: true, // REQUIRED for next/image on GitHub Pages
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
