cd5b24d6e946ed65b14c08250fb6744128050380
Osama Ayub <ayubosama416@gmail.com>
2026-08-08 07:40:44 -0700

    Update data.ts

--- data/data.ts
+++ data/data.ts
@@ -114,7 +114,7 @@ export const projectsData = [
   {
     title: 'AI Powered Interviewer',
     description: 'An AI-powered interview platform that simulates real-world interview scenarios and provides automated feedback.',
-    tags: ['Next Js ', 'Vapi', 'Shadcn', 'Tailwind', 'Firebase'],
+    tags: ['Next Js ', 'Vapi', 'Shadcn', 'Tailwind', 'Firebase','Google Gemini'],
     imageUrl: PrepWise,
     url: 'https://interview-preparation-psi-rosy.vercel.app/sign-in', 
     github: 'https://github.com/osamaayub/Interview-Preparation',
